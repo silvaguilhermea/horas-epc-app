a17d461aab9f94bf413977b0e150e79f27d3a16a
guilhermedasilva <60385200+silvaguilhermea@users.noreply.github.com>
2021-04-14 22:56:46 -0300

    Finalizacao das telas de cadastro das áreas

--- src/app/areas/areas.module.ts
+++ src/app/areas/areas.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { AreasRoutingModule } from './areas-routing.module';
 import { AreasListaComponent } from './areas-lista/areas-lista.component';
@@ -10,7 +11,12 @@ import { AreasFormComponent } from './areas-form/areas-form.component';
   declarations: [AreasListaComponent, AreasFormComponent],
   imports: [
     CommonModule,
-    AreasRoutingModule
+    AreasRoutingModule,
+    FormsModule
+  ],
+  exports: [
+    AreasFormComponent,
+    AreasListaComponent
   ]
 })
 export class AreasModule { }
