522b5824813c1cc874ce63ace282c8a7f122baa1
guilhermedasilva <60385200+silvaguilhermea@users.noreply.github.com>
2021-04-14 21:52:14 -0300

    Implementacao de areas.service.ts

--- src/app/areas.service.ts
+++ src/app/areas.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { Area } from './areas/area';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AreasService {
 
-  constructor() { }
+  apiURL: string = environment.apiURLBase + '/api/areas';
+
+  constructor( private http: HttpClient) { }
+
+  salvar( area: Area ) : Observable<Area> {
+    return this.http.post<Area>( `${this.apiURL}` , area );
+  }
+ 
+  atualizar( area: Area ) : Observable<any> {
+   return this.http.put<Area>(`${this.apiURL}/${area.id}`, area);
+ }
+ 
+  getAreas() : Observable<Area[]> {
+   return this.http.get<Area[]>( this.apiURL );
+  }
+ 
+  getAreaById(id: number) : Observable<Area> {
+    return this.http.get<any>(`${this.apiURL}/${id}`);
+  }
+
+  deletar( area: Area ) : Observable<any> {
+    return this.http.delete<any>(`${this.apiURL}/${area.id}`);
+  }
+  
 }
